6b9f65c4e278d08a1df5283fa70d7bca83bcadbc
James Nornubari <nornubariconfidence@gmail.com>
2024-01-14 19:48:51 +0100

    Updated .env

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from core.event.consume_event import consume_assign_token_event, consume_reused_refresh_token_event, consume_revoke_token_event, consume_update_token_event
 import asyncio
-
+from collections import Counter
 
 async def main():
     await asyncio.gather(
@@ -9,6 +9,5 @@ async def main():
         consume_revoke_token_event(), 
         consume_update_token_event(),
     )
-
 
 asyncio.run(main=main())
